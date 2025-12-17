0db6febb0fb47cb112d7d857fdcd5c866a7a341f
Иванов Илья <danzamaskin@gmail.com>
2025-12-17 18:29:10 +0400

    Initial commit: Add main script with hello world

---
+++ main.py
@@ -0,0 +1,5 @@
+# Это мой первый скрипт на курсе "Введение в инженерную деятельность"
+author_name = "Иванов Илья" 
+print("Hello, World!")
+print(f"I am a future engineer, and my name is {author_name}.")
+print("This project is now on GitHub!")
